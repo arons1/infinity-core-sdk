5d7f9311fa9b15825ac60a8a3def1dd6a7eca67e
Autojadame <autojadame@gmail.com>
2024-01-18 21:51:34 +0100

    eth address

---
+++ src/networks/evm/address/index.ts
@@ -0,0 +1,61 @@
+import {mnemonicToSeedSync,fromSeed,Network,BIP32Interface} from '../../../core'
+import {keccak256} from '../../../core/base'
+import {privateToAddress} from '../sdk/ethereumjs-util';
+
+type MasterNodeParams = {
+    mnemonic:string;
+    network:Network
+}
+type MasterKeyParams = {
+    masterNode:BIP32Interface;
+    bipIdCoin:number;
+    protocol:number;
+}
+type AddressParams = {
+    privateMasterNode:BIP32Interface;
+    change:number;
+    index:number;
+}
+export const getMasterNode = ({
+    mnemonic,
+    network
+}: MasterNodeParams) => {
+    const seed = mnemonicToSeedSync(mnemonic);
+    return fromSeed(seed, network);
+}
+export const getPublicMasterKey = ({
+    masterNode,
+    bipIdCoin,
+    protocol = 39
+}:MasterKeyParams) => {
+    return getPrivateMasterKey({
+        masterNode,
+        bipIdCoin,
+        protocol
+    }).neutered();
+}
+export const getPrivateMasterKey = ({
+    masterNode,
+    bipIdCoin,
+    protocol = 39
+}:MasterKeyParams) => {
+    return masterNode.deriveHardened(protocol)
+    .deriveHardened(bipIdCoin)
+    .deriveHardened(0);
+}
+
+export const getPrivateKey = ({
+    privateMasterNode,
+    change = 0,
+    index = 0
+}:AddressParams) => {
+    return "0x"+ privateMasterNode.derive(change).derive(index).privateKey.toString('hex')
+}
+
+export const getPublicKey = ({
+    privateMasterNode,
+    change = 0,
+    index = 0
+}:AddressParams) => {
+    return privateToAddress(privateMasterNode.derive(change).derive(index).privateKey)
+}
