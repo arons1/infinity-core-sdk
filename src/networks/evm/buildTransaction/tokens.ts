cb4402b4b040aa1b6f59e6db61cee5a8fad7ef44
Autojadame <autojadame@gmail.com>
2024-02-05 16:14:39 +0100

    approve token

--- src/networks/evm/buildTransaction/tokens.ts
+++ src/networks/evm/buildTransaction/tokens.ts
@@ -8,7 +8,7 @@ import ERC20Abi from '../../../core/abi/erc20';
 
 /* 
 buildTokenTransaction
-    Returns a transaction formatted to be sign and send
+    Returns a transfer token transaction formatted to be sign and send
     @param value: ammount to send (optional)
     @param source: source account
     @param destination: destination account
@@ -77,3 +77,75 @@ const getDataTransfer = ({
     });
     return contract.methods.transfer(destination, value).encodeABI();
 }
+
+
+const getDataApprove = ({
+    source,
+    destination,
+    value,
+    tokenContract,
+    web3
+}: DataTransferType) => {
+    var contract = new web3.eth.Contract(ERC20Abi, tokenContract, {
+        from: source,
+    });
+    return contract.methods.approve(destination, value).encodeABI();
+}
+/* 
+buildTokenApproveTransaction
+    Returns a approve transaction formatted to be sign and send
+    @param value: ammount to send (optional)
+    @param source: source account
+    @param destination: destination account
+    @param tokenContract: token contract
+    @param chainId: The ID of the chain
+    @param feeRatio: Between 0 and 1, default 0.5, its the range to increase or decrease de fee, 0.5 = use default gasPrice (optional)
+    @param priorityFee: Just for chainId, 1 or 137, it's used for calculating the fee
+    @param gasPrice: It's the gas price to use (optional). Place it just when you require a fixed gasPrice.
+*/
+export const buildTokenApproveTransaction = async ({
+    value,
+    source,
+    destination,
+    web3,
+    chainId,
+    feeRatio = 0.5,
+    priorityFee,
+    gasPrice,
+    tokenContract
+}: BuildTokenTransaction): Promise<TransactionEVM> => {
+    if (!isValidAddress(source)) throw new Error(InvalidAddress);
+    if (!SupportedChains.includes(chainId)) throw new Error(InvalidChainError);
+    if (!isValidAddress(destination)) throw new Error(InvalidAddress);
+    if (!isValidAddress(tokenContract)) throw new Error(InvalidAddress);
+    if (!gasPrice)
+        gasPrice = await getGasPrice({
+            web3,
+        });
+    const nonce = await getNonce({
+        address: source,
+        web3,
+    });
+    const data = await getDataApprove({
+        source,
+        destination,
+        value,
+        tokenContract,
+        web3
+    })
+    var transaction = {
+        from: source,
+        nonce: nonce,
+        to: tokenContract,
+        data
+    } as TransactionEVM;
+    transaction = await calculateGasPrice({
+        transaction,
+        gasPrice,
+        web3,
+        chainId,
+        feeRatio,
+        priorityFee,
+    });
+    return transaction;
+};
