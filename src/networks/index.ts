22f549c28c2111ab089faae12e67ea9607f2e6c9
Autojadame <autojadame@gmail.com>
2024-02-20 11:19:31 +0100

    cahnges

--- src/networks/index.ts
+++ src/networks/index.ts
@@ -3,4 +3,4 @@ export * as evm from './evm';
 export * as utxo from './utxo';
 export * as registry from './registry';
 export * as derivations from './derivations';
-export * from './getAddress';
+export * from './generate_address';
