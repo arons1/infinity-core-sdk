20140243669dbe9b8aa60f3e00813cf8bb0f1153
Autojadame <autojadame@gmail.com>
2024-03-20 01:03:59 +0100

    d

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as core from './core';
-export * as netwoks from './networks';
+export * as networks from './networks';
 export * as errors from './errors';
 export * as utils from './utils';
