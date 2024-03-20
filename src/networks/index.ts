0e20829e313bf197437f8764ab6be67889f3eae7
Autojadame <autojadame@gmail.com>
2024-03-20 16:35:48 +0100

    utils

--- src/networks/index.ts
+++ src/networks/index.ts
@@ -4,5 +4,7 @@ export * as utxo from './utxo';
 export * as ed25519 from './ed25519';
 export * as registry from './registry';
 export * as derivations from './derivations';
-export * as utils from './utils/secp256k1';
+export * as utils from './utils';
+export * as constants from './constants';
+
 export * from './generate_address';
