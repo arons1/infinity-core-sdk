8259143fdadc49d47488e38a19f3a5b1dc6ef2e0
Autojadame <autojadame@gmail.com>
2024-03-26 13:59:19 +0100

    error codes

--- src/errors/networks/index.ts
+++ src/errors/networks/index.ts
@@ -1,10 +1,10 @@
-export const GenPrivateKeyError = 'generate private key error';
+export const GenPrivateKeyError = 'ERR_GENERATE_PRIVATE_KEY';
 export const MissingExtendedParams =
-    'Missing extended key derivation parameters';
-export const NetworkNotSupported = 'network not supported';
-export const CoinNotSupported = 'coin not supported';
-export const CurveNotSupported = 'curve not supported';
-export const DerivationTypeNotSupported = 'derivation type not supported';
-export const InvalidAddress = 'invalid address';
-export const InvalidMnemonic = 'invalid mnemonic';
-export const DerivePathError = 'derive hdPath error';
+    'ERR_MISSING_EXTENDED_KEY';
+export const NetworkNotSupported = 'ERR_NOT_SUPPORTED_NETWORK';
+export const CoinNotSupported = 'ERR_COIN_NOT_SUPPORTED';
+export const CurveNotSupported = 'ERR_CURVE_NOT_SUPPORTED';
+export const DerivationTypeNotSupported = 'ERR_DERIVATION_TYPE_NOT_SUPPORTED';
+export const InvalidAddress = 'ERR_INVALID_ADDRESS';
+export const InvalidMnemonic = 'ERR_INVALID_MNEMONIC';
+export const DerivePathError = 'ERR_INVALID_DERIVATE_PATH';
