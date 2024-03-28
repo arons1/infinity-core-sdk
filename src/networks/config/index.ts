6a5b4badf5622b8d5b0b5dcbc18e4bba0e223d9d
Autojadame <autojadame@gmail.com>
2024-03-28 21:51:20 +0100

    fv

--- src/networks/config/index.ts
+++ src/networks/config/index.ts
@@ -1,31 +1,35 @@
+import { NotSupported } from '../../errors';
 import { CoinIds, Coins } from '../registry';
 import ECDSABase from './ecdsa';
 import ED25519Base from './ed25519';
 import UTXOBase from './utxo';
-const config: Record<Coins, ED25519Base | UTXOBase | ECDSABase> = {
-    [Coins.BTC]: new UTXOBase(Coins.BTC, CoinIds.BTC),
-    [Coins.LTC]: new UTXOBase(Coins.LTC, CoinIds.LTC),
-    [Coins.DOGE]: new UTXOBase(Coins.DOGE, CoinIds.DOGE),
-    [Coins.GRS]: new UTXOBase(Coins.GRS, CoinIds.GRS),
-    [Coins.FIO]: new ECDSABase(Coins.FIO, CoinIds.FIO),
-    [Coins.STELLAR]: new ED25519Base(Coins.STELLAR, CoinIds.STELLAR),
-    [Coins.XRP]: new ED25519Base(Coins.XRP, CoinIds.XRP),
-    [Coins.ETH]: new ECDSABase(Coins.ETH, CoinIds.ETH),
-    [Coins.BNB]: new ECDSABase(Coins.BNB, CoinIds.BNB),
-    [Coins.MATIC]: new ECDSABase(Coins.MATIC, CoinIds.ETH),
-    [Coins.ONE]: new ECDSABase(Coins.ONE, CoinIds.ETH),
-    [Coins.CRS]: new ECDSABase(Coins.CRS, CoinIds.ETH),
-    [Coins.VET]: new ECDSABase(Coins.VET, CoinIds.ETH),
-    [Coins.SOLANA]: new ED25519Base(Coins.SOLANA, CoinIds.SOLANA),
-    [Coins.TEZOS]: new ED25519Base(Coins.TEZOS, CoinIds.TEZOS),
-    [Coins.AVAX]: new ECDSABase(Coins.AVAX, CoinIds.ETH),
-    [Coins.XDC]: new ECDSABase(Coins.XDC, CoinIds.ETH),
-    [Coins.KCC]: new ECDSABase(Coins.KCC, CoinIds.ETH),
-    [Coins.OKX]: new ECDSABase(Coins.OKX, CoinIds.ETH),
-    [Coins.BSC]: new ECDSABase(Coins.BSC, CoinIds.ETH),
-    [Coins.ARB]: new ECDSABase(Coins.ARB, CoinIds.ETH),
-    [Coins.ETH_TESTNET]: new ECDSABase(Coins.ETH_TESTNET, CoinIds.ETH),
-    [Coins.BSC_TESTNET]: new ECDSABase(Coins.BSC_TESTNET, CoinIds.ETH),
+const config = (idCoin: Coins): ED25519Base | UTXOBase | ECDSABase => {
+    switch(idCoin){
+        case Coins.BTC: return new UTXOBase(Coins.BTC, CoinIds.BTC)
+        case Coins.LTC: return new UTXOBase(Coins.LTC, CoinIds.LTC)
+        case Coins.DOGE: return new UTXOBase(Coins.DOGE, CoinIds.DOGE)
+        case Coins.GRS: return new UTXOBase(Coins.GRS, CoinIds.GRS)
+        case Coins.FIO: return new ECDSABase(Coins.FIO, CoinIds.FIO)
+        case Coins.STELLAR: return new ED25519Base(Coins.STELLAR, CoinIds.STELLAR)
+        case Coins.XRP: return new ED25519Base(Coins.XRP, CoinIds.XRP)
+        case Coins.ETH: return new ECDSABase(Coins.ETH, CoinIds.ETH)
+        case Coins.BNB: return new ECDSABase(Coins.BNB, CoinIds.BNB)
+        case Coins.MATIC: return new ECDSABase(Coins.MATIC, CoinIds.ETH)
+        case Coins.ONE: return new ECDSABase(Coins.ONE, CoinIds.ETH)
+        case Coins.CRS: return new ECDSABase(Coins.CRS, CoinIds.ETH)
+        case Coins.VET: return new ECDSABase(Coins.VET, CoinIds.ETH)
+        case Coins.SOLANA: return new ED25519Base(Coins.SOLANA, CoinIds.SOLANA)
+        case Coins.TEZOS: return new ED25519Base(Coins.TEZOS, CoinIds.TEZOS)
+        case Coins.AVAX: return new ECDSABase(Coins.AVAX, CoinIds.ETH)
+        case Coins.XDC: return new ECDSABase(Coins.XDC, CoinIds.ETH)
+        case Coins.KCC: return new ECDSABase(Coins.KCC, CoinIds.ETH)
+        case Coins.OKX: return new ECDSABase(Coins.OKX, CoinIds.ETH)
+        case Coins.BSC: return new ECDSABase(Coins.BSC, CoinIds.ETH)
+        case Coins.ARB: return new ECDSABase(Coins.ARB, CoinIds.ETH)
+        case Coins.ETH_TESTNET: return new ECDSABase(Coins.ETH_TESTNET, CoinIds.ETH)
+        case Coins.BSC_TESTNET: return new ECDSABase(Coins.BSC_TESTNET, CoinIds.ETH)
+        default: throw new Error(NotSupported)
+    }
 };
 
 export default config;
