d4d293a30de3b3960f570c5136e9cdafde435b81
Autojadame <autojadame@gmail.com>
2024-03-28 22:50:26 +0100

    d

--- src/networks/coin/index.ts
+++ src/networks/coin/index.ts
@@ -4,7 +4,7 @@ import { Coins, Curve } from '../registry';
 import ECDSACoin from './ecdsa';
 import ED25519Coin from './ed25519';
 import SECP256K1Coin from './secp256k1';
-const Config = (idCoin: Coins): ED25519Coin | SECP256K1Coin | ECDSACoin => {
+const Coin = (idCoin: Coins): ED25519Coin | SECP256K1Coin | ECDSACoin => {
     const derivation = derivations[idCoin];
     if (!derivation) throw new Error(CoinNotSupported);
     if (derivation.curve == Curve.ED25519)
@@ -13,4 +13,4 @@ const Config = (idCoin: Coins): ED25519Coin | SECP256K1Coin | ECDSACoin => {
         return new ECDSACoin(idCoin, derivation.bip44);
     return new SECP256K1Coin(idCoin, derivation.bip44);
 };
-export default Config;
+export default Coin;
